2fd5fd72be9c230c03e73edab22ca363bfaba33f
Tandi Samples <tandisamples@gmail.com>
2019-08-09 02:13:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,5 @@
 def 
    colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
-   next_color = "violet"
+   colors_in_the_rainbow
    colors_in_the_rainbow.push(next_color)
 end
